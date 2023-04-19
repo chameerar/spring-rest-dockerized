37dbbd35becd182269334315f06dbf7340b5da02
chameerar <chameerarupasinghe@gmail.com>
2023-04-19 21:37:21 +0530

    package name update

--- src/main/java/io/github/chameerar/springhello/controller/HelloController.java
+++ src/main/java/io/github/chameerar/springhello/controller/HelloController.java
@@ -1,4 +1,4 @@
-package io.github.chameera.springhello.controller;
+package io.github.chameerar.springhello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
